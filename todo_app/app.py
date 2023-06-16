5563f864c2556294e08fd3b4860f70c0749d3ede
Cameron Boddie <cboddie.hudac@onmicrosoft.com>
2023-06-16 13:29:05 +0100

    Added ability to change todo status to complete. Order by completed todos.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, request
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, get_item, add_item, save_item
 
 from todo_app.flask_config import Config
 
@@ -10,10 +10,22 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     todo_items = get_items()
-    return render_template('index.html', todo_items = todo_items)
+    sorted_items = sorted(todo_items, key = lambda item: item['status'], reverse=True)
+    return render_template('index.html', todo_items = sorted_items)
 
-@app.route('/add-todo', methods=['POST'])
+@app.route('/todo/add', methods=['POST'])
 def add_todo():
     new_todo = request.form.get('new-todo-item')
     add_item(new_todo)
+    return redirect('/')
+
+@app.route('/todo/change-status/<id>', methods=['POST'])
+def change_todo_status(id):
+    todo_item = get_item(id)
+    current_status = todo_item['status']
+    new_status = 'Completed' if current_status == 'Not Started' else 'Not Started'
+    todo_item['status'] = new_status
+
+    save_item(todo_item)
+
     return redirect('/')
