212e85baf19b961e190b4ea1bfe8b019022b52e3
Cameron Boddie <cboddie.hudac@onmicrosoft.com>
2023-06-16 14:31:25 +0100

    Added ability to delete todos. Updated css styles. Show number of items left to complete.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, request
-from todo_app.data.session_items import get_items, get_item, add_item, save_item
+from todo_app.data.session_items import get_items, get_item, add_item, save_item, delete_item
 
 from todo_app.flask_config import Config
 
@@ -10,13 +10,21 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     todo_items = get_items()
+    num_todos = len(todo_items)
+    num_complete_todos = len([item for item in todo_items if item['status'] == 'Completed'])
     sorted_items = sorted(todo_items, key = lambda item: item['status'], reverse=True)
-    return render_template('index.html', todo_items = sorted_items)
+    return render_template(
+        'index.html', 
+        todo_items = sorted_items,
+        num_todos = num_todos,
+        num_complete_todos = num_complete_todos
+    )
 
 @app.route('/todo/add', methods=['POST'])
 def add_todo():
     new_todo = request.form.get('new-todo-item')
-    add_item(new_todo)
+    if new_todo.strip() != '':
+        add_item(new_todo)
     return redirect('/')
 
 @app.route('/todo/change-status/<id>', methods=['POST'])
@@ -27,5 +35,12 @@ def change_todo_status(id):
     todo_item['status'] = new_status
 
     save_item(todo_item)
+
+    return redirect('/')
+
+@app.route('/todo/delete/<id>', methods=['POST'])
+def delete_todo(id):
+    todo_item = get_item(id)
+    delete_item(todo_item)
 
     return redirect('/')
