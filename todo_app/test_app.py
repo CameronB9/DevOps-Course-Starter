37b7a36488f1959a9178611dc7a2bec0c7b22505
Cameron Boddie <cboddie.hudac@onmicrosoft.com>
2023-08-11 11:48:34 +0100

    Added integration tests for add, update and delete todo

--- todo_app/test_app.py
+++ todo_app/test_app.py
@@ -6,6 +6,8 @@ import requests
 
 from todo_app import app
 
+TRELLO_BASE_URL = 'https://api.trello.com/1'
+
 @pytest.fixture
 def client():
     # Use our test integration config instead of the 'real' version
@@ -30,10 +32,13 @@ class StubResponse:
 def stub(url, params = {}):
     test_board_id = os.environ.get('TRELLO_BOARD_ID')
 
-    if url == f'https://api.trello.com/1/boards/{test_board_id}/lists':
+    fake_response_data = None
+
+    if url == f'{TRELLO_BASE_URL}/boards/{test_board_id}/lists':
         fake_response_data = [{
             'id': '123abc',
             'name': 'To Do',
+            'idList': '1',
             'cards': [
                 {
                     'id': '456', 
@@ -43,9 +48,15 @@ def stub(url, params = {}):
                 }
             ]
         }]
-        return StubResponse(fake_response_data)
+    elif url == f'{TRELLO_BASE_URL}/cards':
+        fake_response_data = []
+    elif url == f'{TRELLO_BASE_URL}/cards/abc':
+        fake_response_data = { 'idList': '1' }
     
-    raise Exception(f'Integration test did not expect URL {url}')
+    if fake_response_data == None:
+        raise Exception(f'Integration test did not expect URL {url}')
+
+    return StubResponse(fake_response_data)
 
 
 def test_index_page(monkeypatch, client: FlaskClient):
@@ -55,3 +66,31 @@ def test_index_page(monkeypatch, client: FlaskClient):
     assert 'Test card' in decoded_response
     assert 'Test Desc' in decoded_response
     assert '04/08/2023' in decoded_response
+
+def test_add_todo(monkeypatch, client: FlaskClient):
+    monkeypatch.setattr(requests, 'post', stub)
+
+    form_data = {
+        'todo-name': 'Task 1',
+        'todo-description': '',
+        'todo-due-date': ''
+    }
+
+    response = client.post('/todo/add', data = form_data)
+
+    assert response.status_code == 302
+
+def test_update_todo(monkeypatch, client: FlaskClient):
+    monkeypatch.setattr(requests, 'put', stub)
+    monkeypatch.setattr(requests, 'get', stub)
+
+    response = client.post('/todo/change-status/abc')
+
+    assert response.status_code == 302
+
+def test_delete_todo(monkeypatch, client: FlaskClient):
+    monkeypatch.setattr(requests, 'delete', stub)
+
+    response = client.post('/todo/delete/abc')
+
+    assert response.status_code == 302
