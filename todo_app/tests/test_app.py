c0a6f4461fe9b397d492798eb801748c02031de2
Cameron Boddie <cboddie.hudac@onmicrosoft.com>
2024-03-22 13:55:34 +0000

    Updated integration tests to working with mongo mock.

--- todo_app/tests/test_app.py
+++ todo_app/tests/test_app.py
@@ -1,12 +1,15 @@
 from dotenv import load_dotenv, find_dotenv
-from flask.testing import FlaskClient
 import pytest
-import os
-import requests
+from os import getenv
+from datetime import datetime
+from typing import List, Dict
+
+import mongomock
+from flask.testing import FlaskClient
+import pymongo
+
 
 from todo_app import app
-
-TRELLO_BASE_URL = 'https://api.trello.com/1'
 
 @pytest.fixture(scope='module')
 def vcr_config():
@@ -22,71 +25,99 @@ def client():
     file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
 
-    # Create the new app.
-    test_app = app.create_app()
+    with mongomock.patch(servers=(('fakemongo.com', 27017),)):
+        test_app = app.create_app()
+        with test_app.test_client() as client:
+            yield client
 
-    # Use the app to create a test_client that can be used in our tests.
-    with test_app.test_client() as client:
-        yield client
+class MockData:
+    def __init__(self) -> None:
+        self.mongo_client = pymongo.MongoClient(getenv('MONGO_CONNECTION_STRING'))
+        self.db = self.mongo_client['todo-db']
+        self.collection = self.db['todos']
 
-class StubResponse:
-    def __init__(self, fake_response_data):
-        self.fake_response_data = fake_response_data
+    def insert_todos(self, count: int) -> List[str]:
+        data = [
+            {
+                'name': f'Todo Item {i+1}',
+                'description': f'Item {i+1} Description',
+                'is_done': False,
+                'modified_date': datetime.now().isoformat(),
+                'due_date': None
+            }
+            for i in range(count)
+        ]
+        inserted = self.collection.insert_many(data)
 
-    def json(self):
-        return self.fake_response_data
+        return inserted.inserted_ids
 
     
-def stub(url, params = {}):
-    test_board_id = os.environ.get('TRELLO_BOARD_ID')
+    def get_item(self, query: Dict):
+        return self.collection.find_one(query)
 
-    fake_response_data = None
+        
 
-    if url == f'{TRELLO_BASE_URL}/cards':
-        fake_response_data = []
-    elif url == f'{TRELLO_BASE_URL}/cards/abc':
-        fake_response_data = { 'idList': '1' }
-    
-    if fake_response_data == None:
-        raise Exception(f'Integration test did not expect URL {url}')
-
-    return StubResponse(fake_response_data)
-
-@pytest.mark.vcr()
 def test_index_page(client: FlaskClient):
+
+    MockData().insert_todos(10)
+
     response = client.get('/')
     decoded_response = response.data.decode()
+
     assert 'To Do' in decoded_response
     assert 'Completed' in decoded_response
 
-    for i in range(5):
-        assert f'To Do Item {i + 1}' in decoded_response
-        assert f'Completed Item {i + 1}' in decoded_response
+    for i in range(10):
+        assert f'Todo Item {i+1}' in decoded_response
 
-def test_add_todo(monkeypatch, client: FlaskClient):
-    monkeypatch.setattr(requests, 'post', stub)
+
+def test_add_todo(client: FlaskClient):
+
+    task_name = 'TEST_ADD_TASK'
+    mock_data = MockData()
 
     form_data = {
-        'todo-name': 'Task 1',
+        'todo-name': task_name,
         'todo-description': '',
         'todo-due-date': ''
     }
 
     response = client.post('/todo/add', data = form_data)
 
+    query = {
+        'name': task_name
+    }
+
+    added_item = mock_data.get_item(query)
+    assert added_item is not None
     assert response.status_code == 302
 
-def test_update_todo(monkeypatch, client: FlaskClient):
-    monkeypatch.setattr(requests, 'put', stub)
-    monkeypatch.setattr(requests, 'get', stub)
+def test_update_todo(client: FlaskClient):
 
-    response = client.post('/todo/change-status/abc')
+    mock_data = MockData()
+    id = mock_data.insert_todos(1)[0]
 
+    response = client.post(f'/todo/change-status/{str(id)}')
+
+    query = {
+        '_id': id
+    }
+
+    updated_item = mock_data.get_item(query)
+
+    assert updated_item['is_done'] == True
     assert response.status_code == 302
 
-def test_delete_todo(monkeypatch, client: FlaskClient):
-    monkeypatch.setattr(requests, 'delete', stub)
+def test_delete_todo(client: FlaskClient):
+    mock_data = MockData()
+    id = mock_data.insert_todos(1)[0]
+    response = client.post(f'/todo/delete/{id}')
 
-    response = client.post('/todo/delete/abc')
+    query = {
+        '_id': id
+    }
 
+    deleted_item = mock_data.get_item(query)
+
+    assert deleted_item is None
     assert response.status_code == 302
